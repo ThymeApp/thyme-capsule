350ebe6a46e2e1d226af00e5948363693b4fa2f1
Gaya Kessler <gaya.kessler@gmail.com>
2018-05-17 20:39:57 +0200

    add tests for user login

--- src/user.test.js
+++ src/user.test.js
@@ -1,6 +1,8 @@
 import sequelize from './database';
 import { register, login } from './user';
 
+const matchToken = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/;
+
 beforeAll(() => sequelize.sync());
 
 describe('User registration', () => {
@@ -12,10 +14,10 @@ describe('User registration', () => {
 
     const response = await register({ body });
 
-    expect(response).toMatch(/^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/);
+    expect(response).toMatch(matchToken);
   });
 
-  it('Should not be able to register twice', async () => {
+  it('Should not be able to register twice', () => {
     const body = {
       email: 'test@email.com',
       password: 'secret_password',
@@ -24,22 +26,63 @@ describe('User registration', () => {
     expect(register({ body })).rejects.toEqual(new Error('Already a user with email'));
   });
 
-  it('Should reject incomplete requests', async () => {
+  it('Should reject incomplete requests', () => {
     const expectedError = new Error('Missing email / password in request');
 
     expect(register({ body: {} })).rejects.toEqual(expectedError);
     expect(register({ body: { email: 'test@test.com' } })).rejects.toEqual(expectedError);
     expect(register({ body: { password: 'supersecret' } })).rejects.toEqual(expectedError);
-    expect(register({ body: { email: 'test@test.com', password: 123456789 } })).rejects.toEqual(expectedError);
+    expect(register({ body: { email: 'test@test.com', password: 123456789 } }))
+      .rejects.toEqual(expectedError);
   });
 
-  it('Should reject invalid email addresses', async () => {
+  it('Should reject invalid email addresses', () => {
     expect(register({ body: { email: 'invalid', password: 'supersecret' } }))
       .rejects.toEqual(new Error('Invalid email'));
   });
 
-  it('Should reject invalid passwords', async () => {
+  it('Should reject invalid passwords', () => {
     expect(register({ body: { email: 'test@test.com', password: 'super' } }))
       .rejects.toEqual(new Error('Password needs to be at least 6 characters long'));
+  });
+});
+
+describe('User login', () => {
+  (async () => {
+    const body = {
+      email: 'testing@email.com',
+      password: 'secret_password',
+    };
+
+    await register({ body });
+  })();
+
+  it('Should log in user and return a JWT', async () => {
+    const token = await login({
+      body: {
+        email: 'testing@email.com',
+        password: 'secret_password',
+      },
+    });
+
+    expect(token).toMatch(matchToken);
+  });
+
+  it('Should reject incomplete requests', () => {
+    const expectedError = new Error('Missing email / password in request');
+
+    expect(login({ body: {} })).rejects.toEqual(expectedError);
+    expect(login({ body: { email: 'test@test.com' } })).rejects.toEqual(expectedError);
+    expect(login({ body: { password: 'supersecret' } })).rejects.toEqual(expectedError);
+  });
+
+  it('Should fail when user does not exist', () => {
+    expect(login({ body: { email: 'not@found.com', password: 'test' } }))
+      .rejects.toEqual(new Error('Invalid email / password combination'));
+  });
+
+  it('Should fail when password is not correct', () => {
+    expect(login({ body: { email: 'testing@email.com', password: 'wrong_password' } }))
+      .rejects.toEqual(new Error('Invalid email / password combination'));
   });
 });
