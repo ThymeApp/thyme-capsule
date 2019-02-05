548e77fa58ced99c02c0af4eee1980ae5d8b4143
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-05 13:18:42 +0100

    allow users to connect and send changes to their items

--- src/middlewares/socket.js
+++ src/middlewares/socket.js
@@ -2,10 +2,39 @@
 
 import socketIO from 'socket.io';
 
+import { verify } from '../helpers/passport';
+
+function decodeToken(data): null | { id: string } {
+  try {
+    return verify(data.token);
+  } catch (e) {
+    return null;
+  }
+}
+
 export default function connectSocketIO(server: any) {
   const io = socketIO(server, { origins: '*:*' });
 
   io.on('connection', (socket) => {
-    console.log(socket);
+    let userId: string;
+
+    socket.on('connectUser', (data) => {
+      const user = decodeToken(data);
+
+      if (user) {
+        userId = user.id;
+        socket.join(userId);
+      }
+    });
+
+    socket.on('disconnect', () => {
+      if (userId) socket.leave(userId);
+    });
+
+    socket.on('changeItem', (data) => {
+      if (userId && data.item) {
+        io.to(userId).emit('changeItem', { socket: socket.id, item: data.item });
+      }
+    });
   });
 }
