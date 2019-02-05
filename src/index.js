2851b2bfba605c8a4faa00ac39a0e6f08c2ef554
Gaya Kessler <gaya.kessler@gmail.com>
2019-02-05 07:37:17 +0100

    put routes in separate files

--- src/index.js
+++ src/index.js
@@ -6,21 +6,11 @@ import cors from 'cors';
 
 import type { $Request, $Response } from 'express';
 
-import catchError from './helpers/middleware';
 import setupErrorLogging from './helpers/errorLogging';
-import { initialized, authJwt } from './helpers/passport';
+import { initialized } from './helpers/passport';
 import sequelize from './database';
 
-import {
-  login,
-  register,
-  refreshToken,
-  changePassword,
-  accountInformation,
-} from './middlewares/user';
-import { saveJson, retrieveJson } from './middlewares/files';
-import { buySubscription, listSubscriptions, stripeWebhook } from './middlewares/subscription';
-import currentVersion from './middlewares/version';
+import registerRoutes from './routes';
 
 const app = express();
 
@@ -38,27 +28,7 @@ app.use((req: $Request, res: $Response, next) => {
   next();
 });
 
-// App status endpoints
-app.get('/', (req: $Request, res: $Response) => { res.end('Thyme Capsule is running'); });
-
-// App version endpoint
-app.get('/version', currentVersion);
-
-// User endpoints
-app.post('/register', catchError(400, register));
-app.post('/login', catchError(401, login));
-app.post('/refresh-token', authJwt, catchError(401, refreshToken));
-app.post('/change-password', authJwt, catchError(401, changePassword));
-app.get('/account-information', authJwt, catchError(401, accountInformation));
-
-// Subscription endpoints
-app.post('/buy-subscription', authJwt, catchError(401, buySubscription));
-app.get('/list-subscriptions', authJwt, catchError(401, listSubscriptions));
-app.all('/stripe', bodyParser.raw({ type: '*/*' }), stripeWebhook);
-
-// File endpoints
-app.post('/save-state', authJwt, catchError(400, saveJson));
-app.get('/get-state', authJwt, catchError(404, retrieveJson));
+registerRoutes(app);
 
 const port = process.env.PORT || 4000;
 
---
+++ src/routes.js
@@ -0,0 +1,42 @@
+// @flow
+
+import type { $Request, $Response } from 'express';
+import bodyParser from 'body-parser';
+
+import currentVersion from './middlewares/version';
+import {
+  accountInformation,
+  changePassword,
+  login,
+  refreshToken,
+  register,
+} from './middlewares/user';
+import { buySubscription, listSubscriptions, stripeWebhook } from './middlewares/subscription';
+import { retrieveJson, saveJson } from './middlewares/files';
+
+import catchError from './helpers/middleware';
+import { authJwt } from './helpers/passport';
+
+export default function registerRoutes(app: express$Application) {
+  // App status endpoints
+  app.get('/', (req: $Request, res: $Response) => { res.end('Thyme Capsule is running'); });
+
+  // App version endpoint
+  app.get('/version', currentVersion);
+
+  // User endpoints
+  app.post('/register', catchError(400, register));
+  app.post('/login', catchError(401, login));
+  app.post('/refresh-token', authJwt, catchError(401, refreshToken));
+  app.post('/change-password', authJwt, catchError(401, changePassword));
+  app.get('/account-information', authJwt, catchError(401, accountInformation));
+
+  // Subscription endpoints
+  app.post('/buy-subscription', authJwt, catchError(401, buySubscription));
+  app.get('/list-subscriptions', authJwt, catchError(401, listSubscriptions));
+  app.all('/stripe', bodyParser.raw({ type: '*/*' }), stripeWebhook);
+
+  // File endpoints
+  app.post('/save-state', authJwt, catchError(400, saveJson));
+  app.get('/get-state', authJwt, catchError(404, retrieveJson));
+}
